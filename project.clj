2e8df2125ca83b34b6f7fea05372660fed864968
Karsten Schmidt <k@postspectacular.com>
2013-02-28 00:13:23 +0000

    updating project version & deps
    --HG--
    branch : release/0.1.5

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.postspectacular/simplecl "0.1.4"
+(defproject com.postspectacular/simplecl "0.1.5"
   :description "Clojure wrapper & highlevel processing pipeline ops for JOCL/OpenCL."
   :url "http://hg.postspectacular.com/simplecl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [org.clojars.toxi/jocl "2.0.0-rc10"]]
-  :profiles {:test {:dependencies [[com.postspectacular/structgen "0.1.1"]]
+  :profiles {:test {:dependencies [[com.postspectacular/structgen "0.1.2"]]
                     :jvm-opts ["-Xms512m" "-Xmx2g"]}}
 )
