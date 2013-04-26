43084aa21febc069f780c9a29aa29697d7964072
Karsten Schmidt <k@postspectacular.com>
2013-04-27 00:49:48 +0100

    switching to 0.1.7-SNAPSHOT & updating to JOCL-2.0.0-rc11

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject com.postspectacular/simplecl "0.1.6"
+(defproject com.postspectacular/simplecl "0.1.7-SNAPSHOT"
   :description "Clojure wrapper & highlevel processing pipeline ops for JOCL/OpenCL."
   :url "http://hg.postspectacular.com/simplecl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojars.toxi/jocl "2.0.0-rc10"]]
+                 [org.clojars.toxi/jocl "2.0.0-rc11"]]
   :profiles {:test {:dependencies [[com.postspectacular/structgen "0.1.2"]]
                     :jvm-opts ["-Xms512m" "-Xmx2g"]}}
 )
